84ee8ecbea183ac3c343835093848efa6ea1c758
Alexotieno1717 <alexotieno900@gmail.com>
2020-07-14 18:41:13 +0300

    Adde multiply, substraction and divide function

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,41 @@ var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
 alert(result);
+
+
+//Substract
+var substract = function (number1, number2) {
+    return number1 - number2;
+  };
+  var number1 = parseInt(prompt());
+  var number2 = parseInt(prompt());
+  
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var result = substract(number1, number2);
+  alert(result);
+
+  //Multiply
+  var multiply = function (number1, number2) {
+    return number1 * number2;
+  };
+  var number1 = parseInt(prompt());
+  var number2 = parseInt(prompt());
+  
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var result = multiply(number1, number2);
+  alert(result);
+
+
+  //Divide
+  var divide = function (number1, number2) {
+    return number1 / number2;
+  };
+  var number1 = parseInt(prompt());
+  var number2 = parseInt(prompt());
+  
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var result = divide(number1, number2);
+  alert(result);
