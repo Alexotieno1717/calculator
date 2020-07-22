b395d424931350b03c996c028edde0c28c6d160d
Alexotieno1717 <alexotieno900@gmail.com>
2020-07-22 09:14:44 +0300

    Modified js

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,29 @@
-var add = function (number1, number2) {
+// Business (or back-end) logic:
+var add = function(number1, number2) {
   return number1 + number2;
 };
-var number1 = parseInt(prompt());
-var number2 = parseInt(prompt());
+//subtract number
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+//multiply number
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+//Divide
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
 
-
-//Substract
-var substract = function (number1, number2) {
-    return number1 - number2;
-  };
-  var number1 = parseInt(prompt());
-  var number2 = parseInt(prompt());
+// User interface (or front-end) logic:
+$(function(){
+  $('#add').submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($('#add1').val());
+    var number2 = parseInt($('#add2').val());
+    var result = add(number1, number2);
+    $('#output').text(result);
+  })
   
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
-  var result = substract(number1, number2);
-  alert(result);
-
-  //Multiply
-  var multiply = function (number1, number2) {
-    return number1 * number2;
-  };
-  var number1 = parseInt(prompt());
-  var number2 = parseInt(prompt());
-  
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
-  var result = multiply(number1, number2);
-  alert(result);
-
-
-  //Divide
-  var divide = function (number1, number2) {
-    return number1 / number2;
-  };
-  var number1 = parseInt(prompt());
-  var number2 = parseInt(prompt());
-  
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
-  var result = divide(number1, number2);
-  alert(result);
+})
