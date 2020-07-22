759fba0e39ac075265bbdbc497b9825a3625f525
Alexotieno1717 <alexotieno900@gmail.com>
2020-07-22 12:24:46 +0300

    Added substract, divide and multiply calculator

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,8 @@ var divide = function(number1, number2) {
 
 
 // User interface (or front-end) logic:
+
+//Addition
 $(function(){
   $('#add').submit(function(event){
     event.preventDefault();
@@ -26,4 +28,37 @@ $(function(){
     $('#output').text(result);
   })
   
+})
+
+//Substraction
+$(function(){
+  $('#sub').submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($('#sub1').val());
+    var number2 = parseInt($('#sub2').val());
+    var result = subtract(number1, number2);
+    $('#output2').text(result);
+  })
+})
+
+//Divide
+$(function(){
+  $('#divide').submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($('#divide1').val());
+    var number2 = parseInt($('#divide2').val());
+    var result = divide(number1, number2);
+    $('#output3').text(result);
+  })
+})
+
+//Multiplication
+$(function(){
+  $('#mult').submit(function(){
+    event.preventDefault();
+    var number1 = parseInt($('#mult1').val());
+    var number2 = parseInt($('#mult2').val());
+    var result = multiply(number1, number2);
+    $('#output4').text(result);
+  })
 })
